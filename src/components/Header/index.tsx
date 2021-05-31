eb600b7c170a3cee05911b2e070d322dfcedbe74
Charles Azevedo <vx.charles@gmail.com>
2021-05-31 00:37:26 -0300

    Added hamburger menu and active status function and tweaked styles.scss for mobile, plus hamburger menu animations

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,27 @@
+import { useState } from 'react'
 import logoImg from '../../assets/logo.svg'
 import './styles.scss'
 
 export function Header() {
+  const [active, setActive] = useState(false)
+
+  function handleActiveMenu() {
+    return setActive(!active)
+  }
+
   return (
     <header>
       <img src={logoImg} alt="True Change logo" />
-      <nav>
-        <ul className="menu">
+      <div className={active ? "hamburger open" : "hamburger"} onClick={handleActiveMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <nav className={active ? "menu active" : "menu"}>
+        <ul className="menu__ul">
           <li>
             <a href="/#" className="menu__link">Home</a>
           </li>
