b70bf6538557e9de551dc40af4303f62a1f587f9
Charles Azevedo <vx.charles@gmail.com>
2021-05-31 00:20:38 -0300

    Adjusted the Banner styles.scss for mobile and desktop and adjusted the Banner image in index.tsx

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -6,14 +6,16 @@ export function Banner() {
     <section className="banner">
       <div className="banner__description">
         <div className="banner__title">
-          Want anything to be<br /> easy with Truechange.
+          Want anything to be easy with <strong>Truechange.</strong>
         </div>
         <div className="banner__text">
           Provide a network for all your needs with ease and fun using <strong>Truechange</strong> discover interesting features from us.
         </div>
         <button className="banner__btn btn-red" type="button">Get Started</button>
       </div>
-      <img className="banner__img" src={bannerImg} alt="Banner" />
+      <figure className="banner__figure">
+        <img className="banner__img" loading="lazy" src={bannerImg} alt="Banner" />
+      </figure>
     </section>
   )
 }
