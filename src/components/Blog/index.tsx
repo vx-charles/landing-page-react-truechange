82c833c33ce17584a51f4b51fdb482f77283851b
Charles Azevedo <vx.charles@gmail.com>
2021-05-31 00:27:24 -0300

    Adjusted the Blog styles.scss for mobile and desktop, adjusted the loading of the Blog and adjusted the loading of the listing with the rendering of the blog in the index.tsx

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react'
 import { api } from '../../services/api'
 
-import Slider from "react-slick";
-import "../../../node_modules/slick-carousel/slick/slick.css";
-import "../../../node_modules/slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick"
+import "../../../node_modules/slick-carousel/slick/slick.css"
+import "../../../node_modules/slick-carousel/slick/slick-theme.css"
+import loadingImg from '../../assets/loading.svg'
 
 import './styles.scss'
 
@@ -15,19 +16,19 @@ interface BlogProps {
 
 export function Blog() {
 
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(true)
   const [list, setList] = useState<BlogProps[]>([])
 
   useEffect(() => {
     setTimeout(() => {
       api.get('photos')
       .then(resp => setList(resp.data.slice(0,12)))
-      setLoading(true)
+      setLoading(false)
     }, 1300)
   }, [])
 
   const blogList = () => (
-    loading ? list.map(article => (
+    list.map(article => (
       <article key={article.id} className="blog__article">
         <img className="blog__img" src={article.thumbnailUrl} alt={"Thumbnail-" + article.id} />
         <div className="blog__content">
@@ -41,51 +42,45 @@ export function Blog() {
           </div>
         </div>
       </article>
-    ))
-    : <div className="blog__loading">Loading...</div>
+    ))    
   )
 
   const settings = {
     dots: true,
-    infinite: true,
+    infinite: false,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
-    initialSlide: 3,
     responsive: [
       {
         breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        }
-      },
-      {
-        breakpoint: 600,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
         }
       },
       {
-        breakpoint: 480,
+        breakpoint: 768,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 1,
         }
       }
     ]
   }
 
-
   return (
     <section className="blog-container">
       <div className="blog">
         <div className="blog__title">Blog</div>
         <div className="blog__articles">
-          <Slider { ...settings }>
-            {blogList()}
-          </Slider>
+          {
+            !loading ?
+              <Slider { ...settings }>
+                {blogList()}
+              </Slider>
+            : <div className="blog__loading"><img src={loadingImg} alt="Loading..." /></div>
+          }
         </div>
       </div>
     </section>
